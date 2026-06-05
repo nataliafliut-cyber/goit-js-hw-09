da92bd02165da91111e9dc9f3e8940fc83d82a87
Natalia Fliut <natalia.fliut@gmail.com>
2026-06-05 16:22:43 +0200

    ninth js hw

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -2,12 +2,12 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const images = [
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-    description: 'Hokkaido Flower',
+ {
+ preview:
+'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+original:
+ 'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+description: 'Hokkaido Flower',
   },
   {
     preview:
